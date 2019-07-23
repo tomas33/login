a7995411b720604776496c454c8665758f086690
Tomas <tanquealc@gmail.com>
2019-07-23 18:01:39 +0200

    eliminado container de dependencies y añadido a controllers

--- config/controllers.php
+++ config/controllers.php
@@ -5,4 +5,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+use Slim\App;
 
+$container['UserController'] = function ($c) {
+        return new app\controllers\PasswordController($container);
+    };
