978e838b450a2c9309337c8adf5baa845101eff5
Tomas <tanquealc@gmail.com>
2019-07-24 13:00:13 +0200

    añadido use pimple

--- config/routes.php
+++ config/routes.php
@@ -3,13 +3,14 @@
 use Slim\App;
 use Slim\Http\Request;
 use Slim\Http\Response;
+use app\Controllers\HelloWorldController;
 
 return function (App $app) {
     $container = $app->getContainer();
 
     $app->get('/', function (Request $request, Response $response, array $args) use ($container) {
         // Sample log message
-        
+
         $container->get('logger')->addInfo('desde slim.es');
         // Render index view
         return $container->get('renderer')->render($response, 'index.phtml', $args);
