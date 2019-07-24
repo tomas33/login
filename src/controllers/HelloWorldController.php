3e9e9cb9687c3ccd4102b02c0e85d72b4030c4f8
Tomas <tanquealc@gmail.com>
2019-07-24 16:43:48 +0200

    eliminada clase extendida innecesaria

---
+++ src/controllers/HelloWorldController.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace app\controllers;
+
+class HelloWorldController {
+
+    protected $ci;
+
+    public function __construct(ContainerInterface $ci) {
+        $this->ci = $ci;
+    }
+
+    public function __invoke(Request $request, Response $response, $args = []) {
+        return $response->write("Hello World");
+    }
+
+}
