cbe6e4048b63768a9ba9a608737b1242af8b5b22
Tomas <tanquealc@gmail.com>
2019-07-29 15:45:47 +0200

    añadido fonfiguracion dontrine a setting

--- config/settings.php
+++ config/settings.php
@@ -21,6 +21,24 @@ return [
             'pass' => getenv('MYSQL_PASSWORD'),
             'dbname' => getenv('MYSQL_DATABASE')
         ],
+        'doctrine' => [
+            // if true, metadata caching is forcefully disabled
+            'dev_mode' => true,
+            // path where the compiled metadata info will be cached
+            // make sure the path exists and it is writable
+            'cache_dir' => APP_ROOT . '/var/doctrine',
+            // you should add any other path containing annotated entity classes
+            'metadata_dirs' => [APP_ROOT . '/src/Domain'],
+            'connection' => [
+                'driver' => 'pdo_mysql',
+                'host' => 'localhost',
+                'port' => 3306,
+                'dbname' => 'mydb',
+                'user' => 'user',
+                'password' => 'secret',
+                'charset' => 'utf-8'
+            ]
+        ]
     ],
 ];
 
