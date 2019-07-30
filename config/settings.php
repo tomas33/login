d68823d72bb2a10bd8ce585b5a952f3183a06a01
Tomas <tanquealc@gmail.com>
2019-07-30 05:01:02 +0200

    doctrine configurado y funcionando

--- config/settings.php
+++ config/settings.php
@@ -1,7 +1,7 @@
 <?php
 
 use Monolog\Logger;
-
+define('APP_ROOT', __DIR__);
 return [
     'settings' => [
         'displayErrorDetails' => true, // set to false in production
@@ -26,16 +26,16 @@ return [
             'dev_mode' => true,
             // path where the compiled metadata info will be cached
             // make sure the path exists and it is writable
-            'cache_dir' => APP_ROOT . '/var/doctrine',
+            'cache_dir' => APP_ROOT . '../../var/doctrine',
             // you should add any other path containing annotated entity classes
-            'metadata_dirs' => [APP_ROOT . '/src/Domain'],
+            'metadata_dirs' => [APP_ROOT . '../../src/Domain'],
             'connection' => [
                 'driver' => 'pdo_mysql',
                 'host' => 'localhost',
                 'port' => 3306,
-                'dbname' => 'mydb',
-                'user' => 'user',
-                'password' => 'secret',
+                'dbname' => 'login',
+                'user' => 'root',
+                'password' => '',
                 'charset' => 'utf-8'
             ]
         ]
