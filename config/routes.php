2419edd33cf56e8315f71621dae4f4ff1b568329
Tomas <tanquealc@gmail.com>
2019-08-04 17:03:41 +0200

    añadido use a router

--- config/routes.php
+++ config/routes.php
@@ -2,7 +2,7 @@
 
 use Slim\App;
 use App\Controllers\RegisterController;
-
+use App\Controllers\HelloWorldController;
 return function (App $app) {
     $app->get('/hello', HelloWorldController::class);
     $app->post('/registro', RegisterController::class);
