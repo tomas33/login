8f0d379b105b100b6a0bfdfc3616a9bfd73984a9
Sergio <sergio.decandelario@gmail.com>
2019-08-08 23:44:01 +0200

    Añadir PHP-CS (#29)
    Añadido PHP-CS

--- src/Controllers/HelloWorldController.php
+++ src/Controllers/HelloWorldController.php
@@ -6,15 +6,17 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 use Slim\Views\Twig;
 
-class HelloWorldController {
-
+class HelloWorldController
+{
     private $twig;
 
-    public function __construct(Twig $twig) {
+    public function __construct(Twig $twig)
+    {
         $this->twig = $twig;
     }
 
-    public function __invoke(Request $request, Response $response, $args = []) {
+    public function __invoke(Request $request, Response $response, $args = [])
+    {
         return $this->twig->render($response, 'helloworld.twig');
     }
 }
