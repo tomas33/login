1edebfda8ed86ad55824e4697f6db5ce81229a35
Tomas <tanquealc@gmail.com>
2019-08-11 19:01:37 +0200

    probando permisos desde consola linux bash

---
+++ src/Controllers/LoginController.php
@@ -0,0 +1,23 @@
+
+<?php
+
+namespace App\Controllers;
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Slim\Views\Twig;
+
+class LoginController
+{
+    private $twig;
+
+    public function __construct(Twig $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    public function __invoke(Request $request, Response $response, $args = [])
+    {
+        return $this->twig->render($response, 'helloworld.twig');
+    }
+}
