8e4f6b6e842fbf898820280477ca5906a1f53d85
Tomas <tanquealc@gmail.com>
2019-08-11 19:04:08 +0200

    probando permisos desde consola linux bash segunda prueva

--- config/controllers.php
+++ config/controllers.php
@@ -1,10 +1,15 @@
 <?php
 
+use App\Controllers\LoginController;
 use App\Controllers\SignUpController;
 use Doctrine\ORM\EntityManager;
 use Slim\Views\Twig;
 
 $container = $app->getContainer();
+
+$container[LoginController::class] = function ($c) {
+    return new LoginController($c->get(Twig::class));
+};
 
 $container[SignUpController::class] = function ($c) {
     return new SignUpController(
