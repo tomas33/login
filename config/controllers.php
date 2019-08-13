8d92853ba39c10ae72abcc0ee35caa01b75db397
Tomas lloret <tanquealc@hotmail.com>
2019-08-13 22:43:54 +0200

    Usar el FQCN en la definición de los servicios (#33)

--- config/controllers.php
+++ config/controllers.php
@@ -2,12 +2,13 @@
 
 use App\Controllers\SignUpController;
 use Doctrine\ORM\EntityManager;
+use Slim\Views\Twig;
 
 $container = $app->getContainer();
 
 $container[SignUpController::class] = function ($c) {
     return new SignUpController(
         $c->get(EntityManager::class),
-        $c->get("view")
+        $c->get(Twig::class)
     );
 };
