cc90e26b1a6a3ec9c3e5cdfbfeba7557d89816f3
Tomas <tanquealc@hotmail.com>
2019-08-15 01:09:21 +0200

    haciendo cambios en login

--- src/Controllers/LoginController.php
+++ src/Controllers/LoginController.php
@@ -31,7 +31,7 @@ class LoginController
         ($request->getParam('password'),
         PASSWORD_DEFAULT);
         
-        $user = $this->em->getRepository('App\Domain\User')->username();
+        $user = $this->em->getRepository(User::class)->username();
 
         if ($username !=$user["username"])
         {
