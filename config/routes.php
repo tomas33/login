00f30f7e25d5f708f5256e1c8dae744893f557ad
Tomas lloret <tanquealc@hotmail.com>
2019-08-15 21:48:07 +0200

    Login (#38)

--- config/routes.php
+++ config/routes.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Controllers\LoginController;
 use App\Controllers\SignUpController;
 use Slim\App;
 return function (App $app) {
+    $app->post('/login', LoginController::class);
     $app->post('/registro', SignUpController::class);
 };
---
+++ src/Controllers/LoginController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Domain\User;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Slim\Views\Twig;
+class LoginController
+{
+    /**
+     * @var EntityManager
+     */
+    private $em;
+    private $twig;
+
+    public function __construct(EntityManager $em,Twig $twig)
+    {
+        $this->em   = $em;
+        $this->twig = $twig;
+        
+    }
+
+    public function __invoke(Request $request, Response $response, $args = [])
+    {
+        
+        $username = $request->getParam('username');
+        $password = $request->getParam('password');
+        
+        $user = $this->em->getRepository(User::class)->findOneBy([
+    'username' => $username
+]);
+
+        if (is_null($user))
+        {
+            
+          return $this->twig->render($response, 'login-erroneo.html.twig');
+        }
+         if (!password_verify($password,$user->password())) {
+            return $this->twig->render($response, 'login-erroneo.html.twig');
+        }       
+        
+        
+        return $this->twig->render($response, 'login-correcto.html.twig');
+    }
+}
