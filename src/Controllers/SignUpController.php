f8d10904a3c8ce696414b973a600046bdb419b40
Tomas <tanquealc@gmail.com>
2019-08-17 18:25:35 +0200

    cambio de nombre plantilla regiestro.html.twig a base.html.twig crendo comprobacion de email,password,username en SignUpController

--- src/Controllers/SignUpController.php
+++ src/Controllers/SignUpController.php
@@ -8,7 +8,7 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 use Slim\Http\StatusCode;
 use Slim\Views\Twig;
-
+use App\Controller\LoginController;
 class SignUpController
 {
 
@@ -30,12 +30,28 @@ class SignUpController
         $email = $request->getParam('email');
         $crypt  = $request->getParam('password');
         $password = password_hash($crypt, PASSWORD_DEFAULT);
-
+        
+        
         $user = new User($username, $email, $password);
+        $args = [
+            'username'   => FILTER_SANITIZE_ENCODED,
+            'email'      => FILTER_VALIDATE_EMAIL, 
+            'password'   =>['FILTER_CALLBACK'=> LoginController::class]
+        ];
 
-        $this->em->persist($user);
-        $this->em->flush();
+         $user = $this->em->getRepository(User::class)->findOneBy([
+    'username' => $username
+]);
 
-        return $this->twig->render($response, 'mensaje-registro.html.twig');
+        if (filter_var_array($user,$args)){
+            $this->em->persist($user);
+            $this->em->flush();
+
+            return $this->twig->render($response, 'mensaje-registro.html.twig');
+
+        }
+        
+
+        return $this->twig->render($response, 'login-erroneo.html.twig');
     }
 }
--- src/Controllers/UseCases/SignUpUseCase.php
+++ src/Controllers/UseCases/SignUpUseCase.php
@@ -2,25 +2,18 @@
 
 namespace App\Controllers\UseCases;
 
-use App\Domain\User;
-use Slim\Http\Request;
-use Slim\Http\Response;
 use App\controllers\SignUpController;
 class SignUpUseCase
 {
-        public function __invoke(Request $request, Response $response, ?array $args = []): Response
+        public function __invoke($username, $email, $password)
     {
-        $username = $request->getParam('username');
-        $email = $request->getParam('email');
-        $crypt  = $request->getParam('password');
-        $password = password_hash($crypt, PASSWORD_DEFAULT);
-
+    
         $user = new User($username, $email, $password);
 
         $this->em->persist($user);
         $this->em->flush();
 
-        return $this->twig->render($response, 'registro.twig');
+    
     }
 
 }
