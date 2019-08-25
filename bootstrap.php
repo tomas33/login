46dfa6411558be6aeede231b4c677c9e60b9db25
Tomas lloret <tanquealc@hotmail.com>
2019-08-25 22:34:35 +0200

    ISSUE-39 Mejoras en el registro (#41)

--- bootstrap.php
+++ bootstrap.php
@@ -4,13 +4,17 @@ use Slim\App;
 
 require __DIR__.'/./vendor/autoload.php';
 
-$settings = require __DIR__ . '/./config/settings.php';
+$settings       = require __DIR__ . '/./config/settings.php';
 $app = new App($settings);
 
-$dependencies = require __DIR__ . '/./config/dependencies.php';
+$dependencies   = require __DIR__ . '/./config/dependencies.php';
 $dependencies($app);
 
 $routes = require __DIR__ . '/./config/routes.php';
 $routes($app);
 
-$controllers = require __DIR__ . '/./config/controllers.php';
+$controllers    = require __DIR__ . '/./config/controllers.php';
+
+$usecases       = require __DIR__ . '/./config/use-cases.php';
+
+$UserRepository = require __DIR__ . '/./config/repositories.php';
--- src/Controllers/LoginController.php
+++ src/Controllers/LoginController.php
@@ -4,10 +4,10 @@ namespace App\Controllers;
 
 use App\Domain\User;
 use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\EntityManagerInterface;
 use Slim\Http\Request;
 use Slim\Http\Response;
 use Slim\Views\Twig;
+
 class LoginController
 {
     /**
@@ -16,16 +16,14 @@ class LoginController
     private $em;
     private $twig;
 
-    public function __construct(EntityManager $em,Twig $twig)
+    public function __construct(EntityManager $em, Twig $twig)
     {
         $this->em   = $em;
         $this->twig = $twig;
-        
     }
 
     public function __invoke(Request $request, Response $response, $args = [])
     {
-        
         $username = $request->getParam('username');
         $password = $request->getParam('password');
         
@@ -33,14 +31,12 @@ class LoginController
     'username' => $username
 ]);
 
-        if (is_null($user))
-        {
-            
-          return $this->twig->render($response, 'login-erroneo.html.twig');
-        }
-         if (!password_verify($password,$user->password())) {
+        if (is_null($user)) {
             return $this->twig->render($response, 'login-erroneo.html.twig');
-        }       
+        }
+        if (!password_verify($password, $user->password())) {
+            return $this->twig->render($response, 'login-erroneo.html.twig');
+        }
         
         
         return $this->twig->render($response, 'login-correcto.html.twig');
