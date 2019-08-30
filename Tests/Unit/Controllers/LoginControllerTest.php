c98499d2a00736214a00bfeec2c1fd54a91437d4
Tomas <tanquealc@gmail.com>
2019-08-30 13:31:53 +0200

    Method __invoke may not return value of type string, its return declaration is : Psr\Http\Message\ResponseInterface

--- Tests/Unit/Controllers/LoginControllerTest.php
+++ Tests/Unit/Controllers/LoginControllerTest.php
@@ -2,33 +2,36 @@
 
 namespace Tests\Controllers;
 
+
 use App\Controllers\LoginController;
+use App\UseCases\LoginUseCase;
 use PHPUnit\Framework\TestCase;
 use Slim\Views\Twig;
-
 
 class LoginControllerTest extends TestCase
 {
     
     public function testFailure()
     {
-        
         $this->assertClassHasAttribute('useCase', LoginController::class);
         $this->assertClassHasAttribute('twig', LoginController::class);
         $this->assertArrayHasKey('message', ['message' => '$message->getMessage()']);
+    }
 
-        $view = $this->getMockBuilder(Twig::class)
-            ->setMethods(['render'])
+    public function testStub()
+    {
+        $mock = $this->getMockBuilder(LoginController::class)
             ->disableOriginalConstructor()
+            ->disableOriginalClone()
+            ->disableArgumentCloning()
+            ->disallowMockingUnknownTypes()
             ->getMock();
 
-        $response = new Response();
-        $response->write('Rendered text on page');
-        $message[]='';
-        $this->view->expects($this->once())
-            ->method('render')
-            ->with($response, 'login-ko.html.twig', ['message' => $message])
-            ->willReturn($response);
+        $mock->method('__invoke')
+            ->willReturn('foo');
+
+        $controller = new LoginController(LoginUseCase,Twig);
+        $this->assertTrue($controller->twig);
     }
-   
+    
 }
