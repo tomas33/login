102323ebfbb3b78806027865a944a756fb31f41b
Tomas <tanquealc@gmail.com>
2019-08-30 22:09:15 +0200

    Expected type Throwable Found Tests\Controllers\UserAlreadyExistException

--- Tests/Unit/Controllers/LoginControllerTest.php
+++ Tests/Unit/Controllers/LoginControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Controllers;
 
 use App\Controllers\LoginController;
 use App\UseCases\LoginUseCase;
+use Doctrine\DBAL\Schema\View;
 use PHPUnit\Framework\TestCase;
 use Slim\Views\Twig;
 
@@ -13,16 +14,24 @@ class LoginControllerTest extends TestCase
     
     public function testFailure()
     {
-        $observer = $this->getMockBuilder(LoginController::class)
-            ->setMethods(['__invoke'])
-            ->disableOriginalConstructor()
-            ->getMock();
-        $observer->expects($this->once())
+        $loginusecase = $this->createMock(LoginUseCase::class)
             ->method('__invoke')
-            ->with($this->anything()
+            ->will($this->throwException(new UserAlreadyExistException('usuario requerido')));
+            
+
+        $loginusecase->expects($this->once())
+            ->method('render')
+            ->with(
+                $this->anything()
             );
-        $subject = new LoginController('LoginController','email');
-        $subject->attach($observer);
+        $stub = $this->createMock(Twig::class);
+        
+        $stub->method('__invoke')
+            ->willReturn('tomas');
+       
+        $this->assertSame('tomas', $stub->method('__invoke'));
+      
+        
 
     }
     
