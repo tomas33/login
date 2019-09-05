22c470cc2516d22cdd9b790a9153a1a2ad023329
Tomas <tanquealc@gmail.com>
2019-09-05 21:30:37 +0200

    añadido test unitarios SignUpControllerTest

--- Tests/Unit/Controllers/SignUpControllerTest.php
+++ Tests/Unit/Controllers/SignUpControllerTest.php
@@ -1,16 +1,117 @@
 <?php
 
-namespace Tests\Controllers; 
+namespace Tests\Controllers;
 
 use App\Controllers\SignUpController;
+use App\Exceptions\UserAlreadyExistException;
+use App\UseCases\SignUpUseCase;
+use InvalidArgumentException;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Slim\Views\Twig;
 
 class SignUpControllerTest extends TestCase
 {
-    public function testFailure()
+
+    /**
+     * @var LoginUseCase|MockObject
+     */
+    private $useCase;
+
+    /**
+     * @var MockObject|Twig
+     */
+    private $twig;
+
+    /**
+     * @var MockObject|Request
+     */
+    private $request;
+
+    /**
+     * @var MockObject|Response
+     */
+    private $response;
+
+    protected function setUp(): void
     {
-        $this->assertClassHasAttribute('useCase',SignUpController::class);
-        $this->assertClassHasAttribute('twig',SignUpController::class);
-        $this->assertArrayHasKey('message', ['message' => '$message->getMessage()']);
+        parent::setUp();
+
+        $this->useCase  = $this->createMock(SignUpUseCase::class);
+        $this->twig     = $this->createMock(Twig::class);
+        $this->request  = $this->createMock(Request::class);
+        $this->response = $this->createMock(Response::class);
+    }
+
+    private function createSut()
+    {
+        return new SignUpController(
+            $this->useCase,
+            $this->twig
+        );
+    }
+
+    public function useCaseExceptionsProvider(): array
+    {
+        return [
+            [UserAlreadyExistException::class],
+            [InvalidArgumentException::class]
+        ];
+    }
+
+    /**
+     * @dataProvider useCaseExceptionsProvider
+     */
+    public function testFailure(string $exceptionClass)
+    {
+        $exception = $this->createMock($exceptionClass);
+
+        $this->request
+            ->expects($this->exactly(3))
+            ->method('getParam')
+            ->withConsecutive(
+                ['username'],
+                ['email'],
+                ['password']
+            )
+            ->willReturnOnConsecutiveCalls('username', 'email', 'password');
+
+        $this->useCase
+            ->expects($this->once())
+            ->method('__invoke')
+            ->willThrowException($exception);
+
+        $this->twig
+            ->expects($this->once())
+            ->method('render')
+            ->willReturn($this->response);
+
+        $this->createSut()->__invoke($this->request, $this->response);
+    }
+
+    public function testSuccess()
+    {
+
+        $this->request
+            ->expects($this->exactly(3))
+            ->method('getParam')
+            ->withConsecutive(
+                ['username'],
+                ['email'],
+                ['password']
+            )
+            ->willReturnOnConsecutiveCalls('username', 'email', 'password');
+
+        $this->useCase
+            ->expects($this->once())
+            ->method('__invoke');
+
+        $this->twig
+            ->expects($this->once())
+            ->method('render')
+            ->willReturn($this->response);
+        $this->createSut()->__invoke($this->request, $this->response);
     }
 }
