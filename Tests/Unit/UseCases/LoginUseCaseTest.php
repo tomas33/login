24e6858b5324e23ddf9f56db33979412c2862a45
Tomas <tanquealc@gmail.com>
2019-09-11 23:59:55 +0200

    1) LoginUseCaseTest::ExcepcionTest

--- Tests/Unit/UseCases/LoginUseCaseTest.php
+++ Tests/Unit/UseCases/LoginUseCaseTest.php
@@ -1,2 +1,57 @@
 <?php
 
+use App\Exceptions\UserAlreadyExistException;
+use App\Repositories\UserRepository;
+use App\UseCases\LoginUseCase;
+use PHPUnit\Framework\TestCase;
+use SebastianBergmann\ObjectEnumerator\InvalidArgumentException;
+
+class LoginUseCaseTest extends TestCase
+{
+    /**
+     * @var MockObject|repository
+     */
+    private $repository;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->repository = $this->createMock(UserRepository::class);
+        
+    }
+    private function createSut()
+    {
+        return new LoginUseCase(
+            $this->repository
+        );
+    }
+     public function useCaseExceptionsProvider(): array
+    {
+        return [
+            [UserAlreadyExistException::class],
+            [InvalidArgumentException::class]
+        ];
+    }
+   
+    /**
+     * @dataProvider useCaseExceptionsProvider
+     */
+
+    /**
+     * @test
+     */
+    public function ExcepcionTest()
+    {
+        
+        $this->repository
+            ->expects($this->once())
+            ->method('findOneBy')
+            -> with([
+                'username' => 'tomas'
+            ])
+                ;
+        $this->createSut()->__invoke('tomas','email','password');
+    }
+}
+
