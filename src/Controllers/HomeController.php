819f646d508b54068a610249f57946a4c1287f45
Tomas <tanquealc@gmail.com>
2019-09-29 19:47:52 +0200

    corregiendo errores

---
+++ src/Controllers/HomeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controllers;
+
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Slim\Views\Twig;
+
+
+class HomeController
+{
+    
+    private $twig;
+
+
+    public function __construct(Twig $twig)
+    {
+       
+        $this->twig = $twig;
+    }
+
+    public function __invoke(
+        RequestInterface  $request,
+        ResponseInterface $response,
+        ?array $args = []
+    ) 
+    {
+        
+        $this->twig->render($response, 'login-ok.html.twig');
+        
+         
+    }
+}
