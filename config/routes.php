ab0c028e0022ddea12867d24213cc45304b309cd
Tomas <tanquealc@gmail.com>
2019-10-03 00:23:15 +0200

    añadido LogoutController

--- config/routes.php
+++ config/routes.php
@@ -2,10 +2,12 @@
 
 use App\Controllers\HomeController;
 use App\Controllers\LoginController;
+use App\Controllers\LogoutController;
 use App\Controllers\SignUpController;
 use Slim\App;
 return function (App $app) {
     $app->get('/',HomeController::class);
     $app->post('/login', LoginController::class);
+    $app->get('/logout', LogoutController::class);
     $app->post('/registro', SignUpController::class);
 };
---
+++ src/Controllers/LogoutController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controllers;
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Slim\Views\Twig;
+use App\Domain\Session;
+
+class LogoutController
+{
+    private $twig;
+    private $session;
+
+    public function __construct(Twig $twig)
+    {
+
+        $this->twig = $twig;
+
+    }
+
+    public function __invoke(
+        Request $request,
+        Response $response,
+        ?array $args = []
+    ) {
+
+        $this->session = new Session();
+        if ($this->session->getStatus() === 1 || empty($this->session->get('id'))) {
+            $this->session->close();
+            return $response->withRedirect('/', 301);
+        }
+        
+        
+        //return $response->withRedirect('/login', 301);
+    }
+}
