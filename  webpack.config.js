e2136e166bc0bebf422b18e8984c979e9c9fca09
Tomas <tanquealc@gmail.com>
2019-10-12 20:07:05 +0200

    solucionando problemas de permisos

---  webpack.config.js
+++  webpack.config.js
@@ -1,5 +1,15 @@
 const path = require('path');
 module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },
+
     output: {
         path: path.resolve(__dirname, 'public/js'),
     },
