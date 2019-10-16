c8c6dc4b886611c45713eca3ed7f61f05f41cada
Tomas <tanquealc@gmail.com>
2019-10-16 03:00:14 +0200

    añadiendo vue en formularios

---
+++ public/js/script.js
@@ -0,0 +1,26 @@
+﻿const form = new Vue({
+    el: '#form',
+    data: {
+        errors: [],
+        idemail: null,
+        idpasword: null
+    },
+    methods: {
+        checkForm: function (e) {
+            if (this.idemail && this.idpasword) {
+                return true;
+            }
+
+            this.errors = [];
+
+            if (!this.idemail) {
+                this.errors.push('El email es obligatorio.');
+            }
+            if (!this.idpasword) {
+                this.errors.push('la contraseña es obligatoria.');
+            }
+
+            e.preventDefault();
+        }
+    }
+})
